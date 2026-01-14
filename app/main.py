c27efee2e99dc24057ba6d985708c19ab336e0af
Oleg <ototskiy@gmail.com>
2026-01-14 21:41:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -32,5 +32,5 @@ class Carnivore(Animal):
     ) -> None:
         if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
             herbivore.health -= 50
-            if herbivore.health <= 0 and herbivore.alive is not None:
+            if herbivore.health <= 0:
                 Animal.alive.remove(herbivore)
